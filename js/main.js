615c6cd8b0fb389ebd08b2cbfb2ff7434dfa6553
米琪 陳 <miqichen@miqis-MacBook-Pro.local>
2016-08-30 17:52:59 +0800

    第一次上傳

---
+++ js/main.js
@@ -0,0 +1,54 @@
+/*--Original source https://developers.google.com/youtube/iframe_api_reference--*/
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+var videoIdName = '7Y3ZRYQl4Qo';
+
+function onYouTubeIframeAPIReady() {
+	player = new YT.Player('player', {
+		width: '100%',
+		videoId: videoIdName,
+		playerVars: {
+			loop: 1,
+			playlist: videoIdName,
+			controls: 0,
+			iv_load_policy:3,
+			modestbranding:1,
+			showinfo:0,
+			autohide:0
+		},
+		events: {
+			'onReady': onPlayerReady,
+			'onStateChange': onPlayerStateChange
+		}
+	});
+};
+
+function onPlayerReady(event) {
+	event.target.playVideo();
+};
+
+function onPlayerStateChange(event) {
+	event.target.mute();
+};
+
+$(document).ready(function() {
+	$('#play').on('click', function() {
+		player.playVideo();
+	});
+	$('#pause').on('click', function() {
+		player.pauseVideo();
+	});
+	$('#stop').on('click', function() {
+		player.stopVideo();
+	});
+	$('#soundOpen').click(function(){
+		player.unMute();
+	});
+	$('#soundClose').click(function(){
+		player.mute();
+	});
+});
